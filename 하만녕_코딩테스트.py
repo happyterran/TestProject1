3c38057c1359621d897d6c061b6a9121c1ca2727
happyterran <happyterran@yahoo.co.kr>
2023-03-08 21:08:26 +0900

    first commit

---
+++ 하만녕_코딩테스트.py
@@ -0,0 +1,14 @@
+#1. 의좋은 형제
+N1,N2 = map(int,input().split())
+D = int(input())
+arr = [N1,N2]
+
+for i in range(D):
+    if arr[i%2]//2 >1:
+        arr[int(not i%2)] += arr[i%2]//2
+        arr[i%2] -= arr[i%2] //2
+    else:
+        arr[int(not i%2)] += arr[i%2]
+        arr[i%2] -= arr[i%2]
+
+print(arr[0],arr[1])
